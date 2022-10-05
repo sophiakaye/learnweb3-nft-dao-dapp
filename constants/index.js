836c5ded7c49e73a49d9a2c636a2dcd6f8f30b87
Sophia KAYA <sophiakaya@Sophias-MacBook-Pro.local>
2022-10-05 12:30:10 +0300

    first commit

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x3c00D59a4d8319383848b4760aC6b0c03f5aCbc6";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
